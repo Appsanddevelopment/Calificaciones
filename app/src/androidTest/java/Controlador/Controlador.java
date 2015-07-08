3eb5ffac280e0617c9564d265d95dcb0688e1f70
jairocp <andresbbx@gmail.com>
2015-07-08 13:07:18 -0500

    This is a new commit for what I originally planned to be amended

--- app/src/androidTest/java/Controlador/Controlador.java
+++ app/src/androidTest/java/Controlador/Controlador.java
@@ -3,6 +3,7 @@ package Controlador;
 import Negocio.Negocio;
 import Instancia.getbussinesInstance;
 import utylity.utyliti;
+import java.util.ArrayList;
 
 /**
  * Created by macaco on 08/07/2015.
@@ -13,15 +14,28 @@ public class Controlador {
     public Controlador(){
 
     }
+/*
 
-    public String calcularNotaFalta(){
+ARREGLO
+1)POSICION LO QUE LLEVA
+2)posicion lo que necesita
+* **/
+    public ArrayList<String> calcularNotaFalta(float n1,float n2,float n3){
         Negocio n=getbussinesInstance.ObtenerNegocio();
 
-        float nfalta=n.calcularNotaf();
-String nfinal=utyliti.converTonfalta(nfalta);
+        ArrayList<Float> nfalta=n.CalcularNotaFalta(n1,n2,n3);
+ArrayList<String> nfinal=utyliti.converTonfalta(nfalta);
 
         return nfinal;
 
+    }
+
+    public String calcularNotafinal(float n1,float n2,float n3,float Nfinal){
+
+
+        Negocio n=getbussinesInstance.ObtenerNegocio();
+       float total= n.calcularNotaf(n1,n2,n3,Nfinal);
+return "";
     }
 
 }
