dad1633a76b6f349e8ea98923fa4b1d010e2aee0
Emanuel <emartinezpinzon@gmail.com>
2015-07-09 02:54:01 -0500

    Añado algunas funciones en los item's del menú principal != main de la app

--- app/src/main/java/emanuel/calificaciones/principal.java
+++ app/src/main/java/emanuel/calificaciones/principal.java
@@ -4,14 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class principal extends ActionBarActivity {
 
+    EditText nota1, nota2, nota3, examen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        nota1 = (EditText) findViewById(R.id.txtNota1);
+        nota2 = (EditText) findViewById(R.id.txtNota2);
+        nota3 = (EditText) findViewById(R.id.txtNota3);
+        examen = (EditText) findViewById(R.id.txtExamen);
     }
 
     @Override
@@ -29,9 +37,19 @@ public class principal extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_acerca) {
-            return true;
+        if (id == R.id.action_acerca)
+            Toast.makeText(getBaseContext(), "Creado por Jairo y Emanuel", Toast.LENGTH_LONG).show();
+
+        if(id == R.id.action_limpiar){
+            nota1.setText("");
+            nota2.setText("");
+            nota3.setText("");
+            examen.setText("");
         }
+
+        if(id == R.id.action_salir)
+            System.exit(0);
+
 
         return super.onOptionsItemSelected(item);
     }
