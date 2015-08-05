13ca534d85985160f0f4ea65b917ec3dba611a40
Emanuel <emartinezpinzon@gmail.com>
2015-08-05 02:59:15 -0500

    Cambios y prueba

--- app/src/main/java/emanuel/calificaciones/principal.java
+++ app/src/main/java/emanuel/calificaciones/principal.java
@@ -8,7 +8,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
 import Controlador.Controlador;
 
 public class principal extends ActionBarActivity {
@@ -16,26 +18,36 @@ public class principal extends ActionBarActivity {
     Button cmdcalcular;
     Controlador c = new Controlador();
     EditText nota1, nota2, nota3, examen;
+    float n1 = (float) 1.0;
+    float n2 = (float) 1.0;
+    float n3 = (float) 1.0;
+    TextView falta, lleva;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        nota1 = (EditText) findViewById(R.id.txtNota1);
-        nota2 = (EditText) findViewById(R.id.txtNota2);
-        nota3 = (EditText) findViewById(R.id.txtNota3);
-        examen = (EditText) findViewById(R.id.txtExamen);
-
         cmdcalcular=(Button)findViewById(R.id.cmdCalcular70);
-
         cmdcalcular.setOnClickListener(new View.OnClickListener() {
             @Override
-            //aqui programamos  todo
             public void onClick(View v) {
+                /*nota1 = (EditText) findViewById(R.id.txtNota1);
+                nota2 = (EditText) findViewById(R.id.txtNota2);
+                nota3 = (EditText) findViewById(R.id.txtNota3);
+                examen = (EditText) findViewById(R.id.txtExamen);
 
+                n1 = Float.parseFloat(nota1.toString());
+                n2 = Float.parseFloat(nota2.toString());
+                n3 = Float.parseFloat(nota3.toString());*/
+                //ex = Float.parseFloat(examen.toString());
 
-
+                ArrayList<String> result = c.calcularNotaFalta(n1, n2, n3);
+                falta = (TextView) findViewById(R.id.lblNotaExamen);
+                falta.setText(String.valueOf(result.get(0)));
+                lleva = (TextView) findViewById(R.id.lblNotaDefinitiva);
+                lleva.setText(String.valueOf(result.get(1)));
+                Toast.makeText(getBaseContext(), "Falta: "+ result.get(0)+" lleva: "+result.get(1), Toast.LENGTH_LONG).show();
             }
         });
     }
