237658f745216a95ee9085b7661562ebd1de529f
Emanuel <emartinezpinzon@gmail.com>
2015-08-17 02:36:29 -0500

    Jummmm

--- app/src/main/java/LabApps/calificaciones/MainActivity.java
+++ app/src/main/java/LabApps/calificaciones/MainActivity.java
@@ -1,44 +1,23 @@
 package LabApps.calificaciones;
 
-import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
+
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actividad_principal);
-
-        Resources res = getResources();
-
-        TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
-        tabs.setup();
-
-        TabHost.TabSpec spec = tabs.newTabSpec("Bienvenida");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator(""+R.string.title_tab1, res.getDrawable(android.R.drawable.ic_btn_speak_now));
-        tabs.addTab(spec);
-
-        spec = tabs.newTabSpec("Calcular notas");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator(""+R.string.title_tab2, res.getDrawable(android.R.drawable.ic_btn_speak_now));
-        tabs.addTab(spec);
-
-        spec = tabs.newTabSpec("DIVISIST");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator(""+R.string.title_tab3, res.getDrawable(android.R.drawable.ic_btn_speak_now));
-        tabs.addTab(spec);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_actividad_principal, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
